7d41f55d2697d9fae34d177414cff4003c631fcb
Huang Wei <huang@humentsoft.com>
2013-09-11 17:56:37 +0800

    add newer test for render.

--- lib/app_stack.rb
+++ lib/app_stack.rb
@@ -218,14 +218,18 @@ module AppStack
 
   # render from erb if newer
   def render_file!(f, target)
-    # done = 'keep'.white
-    # if newer?(f, target)
-    tilt = Tilt::ERBTemplate.new(f)
-    oh = File.open(target, 'wb')
-    oh.write tilt.render(OpenStruct.new(@attrs.deep_merge(@config['attrs'])))
-    oh.close
-    'rendered'.bold.green
-    # end
+    done = 'keep'.white
+    unless newer?(target, f)
+      tilt = Tilt::ERBTemplate.new(f)
+      oh = File.open(target, 'wb')
+      oh.write tilt.render(OpenStruct.new(@attrs.deep_merge(@config['attrs'])))
+      oh.close
+
+      # update template timestamp
+      FileUtils.touch(f)
+      done = 'rendered'.bold.green
+    end
+    done
   end
 
   # use module variables, skip `new`
